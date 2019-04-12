ec2ead57423cacdcad13976b73c47cb0c2891d7e
Siqi Zhang <iqis.gnahz@gmail.com>
2019-04-12 03:21:27 -0400

    ...

--- abclerk/app.R
+++ abclerk/app.R
@@ -6,14 +6,20 @@ require(scales)
 
 
 #### UI ####
-ui <- navbarPage("A/B Clerk: A/B Power Calculator", theme = shinytheme("yeti"),
-        tabPanel("Visitor vs Power",
+ui <- navbarPage("A/B Clerk v0.0.2: Test Calculator", theme = shinytheme("yeti"),
+        tabPanel("How many visitors?",
         sidebarLayout(
-        sidebarPanel(
+        sidebarPanel(width = 3,
                 h4("Test Configuration"),
                 hr(),
                 numericInput("p1",
                              "Plan A Conversion Rate %",
+                             value = 5,
+                             min = 0,
+                             max = 100
+                ),
+                numericInput("p2",
+                             "Plan B Conversion Rate %",
                              value = 5,
                              min = 0,
                              max = 100
@@ -24,35 +30,48 @@ ui <- navbarPage("A/B Clerk: A/B Power Calculator", theme = shinytheme("yeti"),
                              min = 0
                 ),
                 br(),
-                h4("Desired Statistics"),
+                h4("Desired Statistic Levels"),
                 hr(),
-                selectInput("confidence",
-                             "Confidence %",
-                            choices = c(90, 95, 99, 99.9),
-                            selected = 95,
-                            selectize = FALSE
-                ),
                 sliderInput("desired_power",
                              "Power",
                              value = 0.8,
                              min = 0.5,
                              max = 1,
                              step = 0.05
+                ),
+                radioButtons("confidence",
+                             "Confidence",
+                            choiceValues = c(90, 95, 99, 99.9),
+                            choiceNames = c("90%", "95%", "99%", "99.9%"),
+                            selected = 95,
+                            inline = TRUE
                 )
         ),
         mainPanel(
-                h2("How many visits do I need?"),
-                plotOutput("plot_01",
-                           click = clickOpts("plot_01_click")),
-                textOutput("conclusion"),
-                verbatimTextOutput("xxx")
-        ))
-))
+                h2("How many visitors do I need?"),
+                br(),
+                fluidRow(column(2), column(7, htmlOutput("conclusion"))),
+                br(),
+                fluidRow(column(1),
+                         column(10,
+                                h4("Visitor/Power"),
+                                plotOutput("plot_01",
+                                           click = clickOpts("plot_01_click"))
+                )),
+                br()
+        ))),
+        tabPanel("Test result significant?",
+        sidebarLayout(
+        sidebarPanel(
+                ),
+        mainPanel(
 
+        )))
+)
 #### Server ####
 server <- function(input, output) {
 
-        n <- seq(250, 100000, by = 100)
+        n <- seq(100, 100000, by = 100)
         p1 <- reactive(input$p1 / 100)
         uplift <- reactive(input$uplift / 100)
         p2 <- reactive(p1()*(1 + uplift()))
@@ -79,7 +98,8 @@ server <- function(input, output) {
                         geom_line(size = 1.5, color = "deepskyblue3") +
                         scale_y_continuous(name = NULL,
                                            labels = percent
-                        )
+                        ) +
+                        xlab(NULL)
         })
 
         output$plot_01 <- renderPlot({
@@ -100,8 +120,10 @@ server <- function(input, output) {
 
         output$conclusion <- renderText({
                 req(power)
-                paste("You need", necessary_n(), "visits to make sure that your A/B test has a", percent(desired_power()), "probability to confirm whether Plan B will produce a", percent(uplift()), "uplift from Plan A's", percent(p1()), "conversion rate, owing less than", percent(sig(), accuracy = 0.01), "to random chance.")})
-        output$xxx <- renderPrint(input$plot_01_click$x)
+                with(tags,
+                paste("... you need at least", b(necessary_n()), "visitors for each plan to make sure that your A/B test has a", percent(desired_power()), "probability to confirm whether Plan B will produce a", percent(uplift()), "uplift from Plan A's", percent(p1()), "conversion rate, owing less than", percent(sig(), accuracy = 0.1), "to random chance."))
+        })
+        output$xxx <- renderText(input$plot_01_click$x)
 }
 
 # Run the application
